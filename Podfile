9b1707b8ea2adec1fb065c75ac42abdde0ef3088
Zin Min Phyo <zinminphyo075@gmail.com>
2020-02-25 23:35:53 +0630

    First commit

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'aws' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+    pod 'amplify-tools'
+
+    pod 'Amplify'
+    pod 'AWSPluginsCore'
+    pod 'AmplifyPlugins/AWSAPIPlugin'
+
+  # Pods for aws
+
+end
---
+++ aws/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aws
+//
+//  Created by Zin Min on 25/02/2020.
+//  Copyright © 2020 Zin Min Phyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
